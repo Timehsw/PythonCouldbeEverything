764c26cb09d06d180ed4cd153e0194c92adbb8cf
HswTime <hsw.time@gmail.com>
2017-07-25 11:45:07 +0800

    上传糗事百科段子小爬虫

--- PythonModels2/XpathSpider/qiushi.py
+++ PythonModels2/XpathSpider/qiushi.py
@@ -1,45 +1,54 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
+'''
+    Created by hushiwei on 2017/7/25.
+'''
+'''
+获取糗事百科的段子
+'''
 
-import urllib2
+import requests
+from lxml import html
 import json
-from lxml import etree
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+class Qiushi():
+    def __init__(self):
+        self.url = "http://www.qiushibaike.com/8hr/page/"
+        self.headers = {"User-Agent": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;"}
+    def loagPage(self,page):
+        url=self.url+str(page)+"/"
+        text=requests.get(url,headers=self.headers).text
+        content_text=html.fromstring(text)
 
-url = "http://www.qiushibaike.com/8hr/page/2/"
-headers = {"User-Agent" : "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;"}
+        # 获取所有的段子节点
+        node_list=content_text.xpath('//div[contains(@id,"qiushi_tag")]')
+        for node in node_list:
+            username= node.xpath('.//h2/text()')[0].encode('utf-8')
+            content=node.xpath('.//div[@class="content"]/span/text()')[0]
+            vote=node.xpath('./div[@class="stats"]/span[@class="stats-vote"]/i/text()')[0]
+            commentNum=node.xpath('./div[@class="stats"]/span[@class="stats-comments"]//i/text()')[0]
+            item={
+                "username":username,
+                "content":content,
+                "vote":vote,
+                "commentNum":commentNum
+            }
+            with open('qiushiduanzi.json','a') as f:
+                f.write(json.dumps(item,ensure_ascii=False)+"\n")
 
-request = urllib2.Request(url, headers = headers)
 
-html = urllib2.urlopen(request).read()
-# 响应返回的是字符串，解析为HTML DOM模式 text = etree.HTML(html)
-
-text = etree.HTML(html)
-# 返回所有段子的结点位置，contains()模糊查询方法，第一个参数是要匹配的标签，第二个参数是标签名部分内容
-node_list = text.xpath('//div[contains(@id, "qiushi_tag")]')
-
-items ={}
-for node in node_list:
-    # xpath返回的列表，这个列表就这一个参数，用索引方式取出来，用户名
-    username = node.xpath('./div/a/@title')[0]
-    # 图片连接
-    image = node.xpath('.//div[@class="thumb"]//@src')#[0]
-    # 取出标签下的内容,段子内容
-    content = node.xpath('.//div[@class="content"]/span')[0].text
-    # 取出标签里包含的内容，点赞
-    zan = node.xpath('.//i')[0].text
-    # 评论
-    comments = node.xpath('.//i')[1].text
-
-    items = {
-        "username" : username,
-        "image" : image,
-        "content" : content,
-        "zan" : zan,
-        "comments" : comments
-    }
-
-    with open("qiushi.json", "a") as f:
-        f.write(json.dumps(items, ensure_ascii = False).encode("utf-8") + "\n")
+    def workon(self):
+        pages=input("请输入你要爬取的页数:")
+        for page in range(1,pages+1):
+            self.loagPage(page)
+            print "第%d页爬取完成" % page
+        print "全部爬取完成..."
 
 
 
+
+
+if __name__ == '__main__':
+    spider=Qiushi()
+    spider.workon()
